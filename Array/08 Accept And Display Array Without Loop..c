6d4ebd415ae25502652c7fc487bebe47268e385a
sanika0201 <sanikayadav1436@gmail.com>
2024-02-09 18:58:30 +0530

    Practice Programs

---
+++ Array/08 Accept And Display Array Without Loop..c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int Num[4]={};
+
+    printf("\n__________________________________________\n");
+
+    printf("\n\nElements Of an Array 111. = %d.",Num[0]);
+
+    printf("\n\nElements Of an Array 211. = %d.",Num[1]);
+
+    printf("\n\nElements Of an Array 311. = %d.",Num[2]);
+
+    printf("\n\nElements Of an Array 411. = %d.",Num[3]);
+
+
+    printf("\n\n__________________________________________\n");
+
+    getch();
+
+    system("cls");
+
+    printf("\n___________________________________________\n");
+
+    printf("\n Enter Four Elements Of an Array.=>");
+
+    printf("\n___________________________________________\n");
+
+
+    printf("\n Enter First Element Of Array.:- ");
+    scanf("%d",&Num[0]);
+
+    printf("\n Enter Second Element Of Array.:- ");
+    scanf("%d",&Num[1]);
+
+    printf("\n Enter Third Element Of Array.:- ");
+    scanf("%d",&Num[2]);
+
+    printf("\n Enter Fourth Element Of Array.:- ");
+    scanf("%d",&Num[3]);
+
+    printf("\n___________________________________________\n");
+
+
+    getch();
+
+    system("cls");
+
+    printf("\n___________________________________________\n");
+
+    printf("\n Given Elements Of An Array ::-");
+
+    printf("\n___________________________________________\n");
+
+    printf("\n\nElements Of an Array 101. = %d.",Num[0]);
+
+    printf("\n\nElements Of an Array 201. = %d.",Num[1]);
+
+    printf("\n\nElements Of an Array 301. = %d.",Num[2]);
+
+    printf("\n\nElements Of an Array 401. = %d.",Num[3]);
+
+    printf("\n___________________________________________\n");
+
+
+
+    return 0;
+
+}
