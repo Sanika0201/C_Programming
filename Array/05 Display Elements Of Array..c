783723e708b36e231f6fa26e5910a69fe3f58413
sanika0201 <sanikayadav1436@gmail.com>
2024-02-09 18:56:55 +0530

    Practice Programs

---
+++ Array/05 Display Elements Of Array..c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+
+    int Num[4]={3,90,7};
+
+    printf("\nElements Of an Array 1= %d",Num[0]);
+
+    printf("\nElements Of an Array 2= %d",Num[1]);
+
+    printf("\nElements Of an Array 3= %d",Num[2]);
+
+    printf("\nElements Of an Array 4= %d",Num[3]);
+
+
+        getch();
+        return 0;
+}
